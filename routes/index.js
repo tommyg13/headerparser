d25c508129948e61d7890a71c5e2760dbfea4fe0
thomas <thomasgaz@hotmail.com>
2017-01-18 14:10:38 +0000

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  
+  var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+     var info = {
+         'ipaddress': ip,
+         'language': req.headers["accept-language"].split(',')[0],
+         'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+         
+     };
+     res.send(info);
 });
 
 module.exports = router;
